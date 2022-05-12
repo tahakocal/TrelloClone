0be66d4bf0006631d58742570b9e513c08ca2b0c
tahakocal <tahakocalgs@gmail.com>
2022-05-12 16:48:40 +0300

    12.05.2022

--- WebApplication12/Models/ToDo.cs
+++ WebApplication12/Models/ToDo.cs
@@ -19,6 +19,7 @@ namespace WebApplication12.Models
         public string Description { get; set; }
         public int StatusId { get; set; }
         public int UserId { get; set; }
+        public Nullable<int> IsActive { get; set; }
     
         public virtual Status Status { get; set; }
         public virtual User User { get; set; }
